f3f099c0e2ec9ee5405d42fb305ba454ba98ced2
sriaparna70-maker <sriaparna70@gmail.com>
2025-08-21 03:15:57 -0900

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,8 @@ def contact():
     sent = send_email_via_zoho(name, email, message)
 
     return jsonify({ "ok": True, "id": lead_id, "created_at": created_at, "email_sent": bool(sent) })
+init_db()
 
 if __name__ == "__main__":
-    init_db()
+    
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", "8080")), debug=True)
